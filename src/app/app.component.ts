86e27610a98ea62cd1532c35d25b5a0335c01859
kunjesh1 <kumarkunjesh1@gmail.com>
2019-07-31 21:01:03 +0530

    adding git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
   title = 'client';
+
+  afuConfig = {
+    uploadAPI: {
+      url:"http://localhost:3000/file",
+      headers: {
+     'Content-Type': 'application/json'
+        
+         },
+         body:FormData,
+         theme: "dragNDrop",
+    hideProgressBar: true,
+    hideResetBtn: true,
+    hideSelectBtn: true
+    }
+};
 }
